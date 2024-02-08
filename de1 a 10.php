9320c1aca1bd9df2c4bd112c4474dae86ccf2178
Felipe Lenz <134728459+Fel1peZ@users.noreply.github.com>
2024-02-08 15:41:54 -0300

    Create de1 a 10.php

---
+++ de1 a 10.php
@@ -0,0 +1,21 @@
+<?php
+$a = 0;
+$b = 0;
+while ($a != 11) {
+   echo $a;
+   $a = $a + 1;
+   echo "\n" ;
+}
+echo "\n" ;
+do{
+   echo $b;
+   $b = $b + 1;
+   echo "\n" ;
+}while ($b != 11);
+echo "\n" ;
+
+for ($i=0; $i < 11; $i++) { 
+   echo $i;
+   echo "\n" ;
+}
+echo "\n" ;
